16bb3a897716ac3c26e701ce993539015b7e9077
chunrichi <545049148@qq.com>
2018-08-19 22:49:58 +0800

    update
    添加课表存储

--- pages/class/class.js
+++ pages/class/class.js
@@ -9,21 +9,8 @@ Page({
         uid: '',
         pwd: '',
         colorArrays: ["#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD", '#85C69D'],
-        wlist1: [
-            { "xqj": 1, "skjc": 1, "skcd": 4, "kcmc": "高等数学@教A-301" },
-            { "xqj": 1, "skjc": 5, "skcd": 3, "kcmc": "高等数学@教A-301" },
-            { "xqj": 2, "skjc": 1, "skcd": 2, "kcmc": "高等数学@教A-301" },
-            { "xqj": 2, "skjc": 8, "skcd": 2, "kcmc": "高等数学@教A-301" },
-            { "xqj": 3, "skjc": 4, "skcd": 1, "kcmc": "高等数学@教A-301" },
-            { "xqj": 3, "skjc": 8, "skcd": 1, "kcmc": "高等数学@教A-301" },
-            { "xqj": 3, "skjc": 5, "skcd": 2, "kcmc": "高等数学@教A-301" },
-            { "xqj": 4, "skjc": 2, "skcd": 3, "kcmc": "高等数学@教A-301" },
-            { "xqj": 4, "skjc": 8, "skcd": 2, "kcmc": "高等数学@教A-301" },
-            { "xqj": 5, "skjc": 1, "skcd": 2, "kcmc": "高等数学@教A-301" },
-            { "xqj": 6, "skjc": 3, "skcd": 2, "kcmc": "高等数学@教A-301" },
-            { "xqj": 6, "skjc": 7, "skcd": 3, "kcmc": "高等数学@教A-301" },
-            { "xqj": 7, "skjc": 5, "skcd": 3, "kcmc": "高等数学@教A-301" }],
-        wlist:[]
+        wlist: [],
+        ifGetNewClass: false
     },
 
     /**
@@ -31,23 +18,44 @@ Page({
      */
     onLoad: function(options) {
         var that = this;
+        var today = new Date();
+        var getwlist = wx.getStorageSync('wlist');
+        var lastDay = wx.getStorageSync('lastDay');
+        // var lastDay = new Date(that.data.lastRequestsTime);
         that.setData({
             uid: app.globalData.uid,
             pwd: app.globalData.pwd,
-        });
-        wx.request({
-            url: 'http://127.0.0.1:5000/wechart/CqClassBox/getClass/uid=201503306&pwd=201503306',
-            // url: 'http://39.107.243.115:5000/wechart/CqClassBox/getClass/uid=201503306&pwd=201503306',
-            method: 'POST',
-            success: function(res) {
-                console.log(res.data);
-                that.setData({
-                    wlist:res.data,
-                })
-            },
-            fail: function(res) {},
-            complete: function(res) {},
-        })
+            wlist: getwlist,
+        });   
+        if (getwlist != '') {
+            console.log(getwlist)
+            // lastDay.getDate() -7 <= today.getDate()
+            if (today.getDate() - 7 >= lastDay.getDate() || today.getDay() < lastDay.getDay()) {
+                console.log(that.data.lastRequestsTime)
+                that.data.ifGetNewClass = true;
+            }
+        } else {
+            that.data.ifGetNewClass = true;
+        }
+        if (that.data.ifGetNewClass) {
+            wx.request({
+                url: 'http://127.0.0.1:5000/wechart/CqClassBox/getClass/uid=201503306&pwd=201503306',
+                // url: 'http://39.107.243.115:5000/wechart/CqClassBox/getClass/uid=201503306&pwd=201503306',
+                method: 'POST',
+                success: function(res) {
+                    var day = new Date()
+                    console.log(res.data);
+                    that.setData({
+                        wlist: res.data
+                    })
+                    // 设置课表的本地缓存，用来下次进入时候使用
+                    wx.setStorageSync('wlist', res.data);
+                    wx.setStorageSync('lastDay', new Date())
+                },
+                fail: function(res) {},
+                complete: function(res) {},
+            });
+        }
     },
 
     /**
